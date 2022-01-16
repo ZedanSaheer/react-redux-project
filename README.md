f0237fd084a9a704b952de1e9c0e64a90e2a286b
Zedan Saheer <zed.saheer5@gmail.com>
2022-01-16 10:51:47 +0530

    understood redux toolkit and slices

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Redux Based React Project
 
 This project puts emphasis on redux based state managaement for practice or revision purpose.
+
+![screenshot](ss.png)
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../features/user';
 
 const Login = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <button>Login</button>
+            <button onClick={() => {
+                dispatch(login({
+                    name: "Zedan Saheer", age: 21, email: "zed.saheer5@gmail.com"
+                }));
+            }}>Login</button>
         </div>
     )
 }
